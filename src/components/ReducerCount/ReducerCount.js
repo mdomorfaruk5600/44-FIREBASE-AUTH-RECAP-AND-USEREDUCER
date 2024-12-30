257e2fb2da5a199f469a4cc4acd1b7b56923d3da
mdomorfaruk5600 <md.omorfaruk5600@gmail.com>
2024-12-30 16:41:39 +0600

    44-8 use useReducer for a simple case

--- src/components/ReducerCount/ReducerCount.js
+++ src/components/ReducerCount/ReducerCount.js
@@ -3,7 +3,14 @@ import React, { useReducer, useState } from 'react';
 const initialState = {count: 0};
 
 const reducer = (state, action) => {
-
+    switch (action.type) {
+        case "INCREMENT":
+            return {count: state.count + 1};
+        case "DECREMENT":
+            return {count: state.count - 1};
+        default:
+            return state;
+    }
 }
 
 const ReducerCount = () => {
@@ -11,7 +18,9 @@ const ReducerCount = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <div>
-            <h1>This is Reducer Count</h1>
+            <h1>This is Reducer Count: {state.count}</h1>
+            <button onClick={()=>dispatch({type:"INCREMENT"})}>increment</button>
+            <button onClick={()=>dispatch({type:"DECREMENT"})}>Decrement</button>
         </div>
     );
 };
